23520886ab78b45177d1f7baf2dad514cecc7bef
tng <tangorri@live.fr>
2017-07-18 13:25:28 +0200

    FIX affichage du nom de l'utilsateur

--- src/pages/accueil/accueil.ts
+++ src/pages/accueil/accueil.ts
@@ -5,7 +5,11 @@ import { NavController } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireDatabase } from 'angularfire2/database';
 
-
+// modèle pour User
+class User {
+  username:string;
+  mail:string;
+}
 
 @Component({
   selector: 'page-accueil',
@@ -13,16 +17,23 @@ import { AngularFireDatabase } from 'angularfire2/database';
 })
 export class AccueilPage {
 
+  // le profil de l'utilistateur
+  user:Object;
+  // indiquer à la vue si les données sont chargée.
+  profileLoaded:boolean = false;
 
   constructor(public navCtrl: NavController , private afAuth: AngularFireAuth, private dbAf: AngularFireDatabase ) {
 
     var userId = afAuth.auth.currentUser.uid;
     console.log('userId: '+userId);
 
-    dbAf.database.ref('/users/'+ userId).once('value').then(function(snapshot) {
-      var username = snapshot.val().username;
-      console.log('username: '+username);
-    }); 
+    dbAf.database.ref('/users/'+ userId).once('value').then((snapshot) => {
+      // @TODO à placer dans un Service pour le rendre accessible globalement
+      // dans  aufAuth.auth.currentUser par exemple ?
+      this.user = snapshot.val() as User;
+      console.log('username: '+ this.user);
+      this.profileLoaded = true;
+    });
 
   }
 
