5924b13ee28818606e1613daff3f26438106bc13
Kris <chrisparis.eh@gmail.com>
2017-07-19 22:54:16 +0200

    css inventaire

--- src/pages/ramassage/ramassage.ts
+++ src/pages/ramassage/ramassage.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+// modèle pour Marchandise
+class Marchandise {
+  reference:number;
+  designation:string;
+  quantite:number;
+  poids:number;
+  prix:number;
+  expediteur:string;
+  destinataire:string;
+}
+
 @Component({
   selector: 'page-ramassage',
   templateUrl: 'ramassage.html'
 })
 export class RamassagePage {
 
+  // le modèle des marchandises
+  marchandise: FirebaseListObservable<any>;
   reference: number;
   designation: string;
   quantite: number;
@@ -15,26 +31,20 @@ export class RamassagePage {
   expediteur: string; 
   destinataire: string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController , private afAuth: AngularFireAuth, private dbAf: AngularFireDatabase) {
 
-  }
-
-  public setObject(): void {
-    console.log('reference : ' + this.reference);
-    console.log('designation : ' + this.designation);
-    console.log('quantite : ' + this.quantite);
-    console.log('poids : ' + this.poids);
-    console.log('prix : ' + this.prix);
-    console.log('expediteur : ' + this.expediteur);
-    console.log('destinataire : ' + this.destinataire);
-
-    alert('La référence n° ' + this.reference + ' à bien été ajoutée !' +
-          ' designation : ' + this.designation + 
-          ' quantite : ' + this.quantite +
-          ' poids : ' + this.poids + 
-          ' prix : ' + this.prix +
-          ' expediteur : ' + this.expediteur +
-          ' destinataire : ' + this.destinataire);
+      /* const Marchandise = this.dbAf.object('/marchandise');
+      console.log(Marchandise); 
+      
+      Marchandise.push({ 
+        reference: this.reference,
+        designation: this.designation,
+        quantite: this.quantite,
+        poids: this.poids,
+        prix: this.prix,
+        expediteur: this.expediteur,
+        destinataire: this.destinataire
+      }); */
   }
 
 }
