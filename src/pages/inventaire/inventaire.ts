d24e9faddf5dc9f1a8c2464f5c2d20d7fd1551b8
Kris <chrisparis.eh@gmail.com>
2017-07-24 20:47:40 +0200

    ajout suppression depuis inventaire

--- src/pages/inventaire/inventaire.ts
+++ src/pages/inventaire/inventaire.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 import { Loader } from '../../services/loader/loader';
 
@@ -11,6 +12,7 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 
 // modèle pour Marchandises
 class Marchandise {
+  key:string;
   reference:number;
   designation:string;
   quantite:number;
@@ -36,19 +38,59 @@ class Client {
 
 export class InventairePage {
   // le modèle des marchandises
-  marchandise: FirebaseListObservable<any>;
+  marchandise: any;
   // le modèle des clients
   client: FirebaseListObservable<any>;
 
-  constructor(public navCtrl: NavController , private afAuth: AngularFireAuth, private dbAf: AngularFireDatabase, public loader: Loader, private utilisateurProvider: UtilisateurProvider ) {
+
+  constructor(public navCtrl: NavController , private afAuth: AngularFireAuth, private dbAf: AngularFireDatabase, public loader: Loader, private utilisateurProvider: UtilisateurProvider, public alertCtrl: AlertController) {
 
     var user = firebase.auth().currentUser.uid;
     this.marchandise = dbAf.list('/users/' + user + '/cargaison/');
     
   }
 
-  coucou() {
-    alert('COUCOU je ne suis pas encore programmé !')
-  }
+  suppr(key:string) {
+    // Confirmer la suppression
+    let confirm = this.alertCtrl.create({
+      title: 'SUPPRIMER ?',
+      message: 'Etes vous vraiment sûr de vouloir supprimer ce bon de livraison ?',
+      buttons: [
+        {
+          text: 'Annuler',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Supprimer',
+          handler: () => {
+            // Supprimer de firebase
+            this.marchandise.remove(key).then(snapshot => {
+              alert( "Ce bon de livraison à bien été supprimé");
+            })
+            .catch(error => {
+              console.log( "Une erreur est survenue !");
+            });
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }; 
+
+
+  /* modifier(key:string) {
+    this.marchandise.update(key).then(snapshot => {
+      console.log( "Ce bon de livraison n°" + this.marchandise.reference + "à bien été modifié");
+    })
+    .catch(error => {
+      console.log( "Une erreur est survenue !");
+    }) */
+
 
 }
+
+
+
+
